99223c4a9e67fced4734c1ce7a343411582666ad
Julian Porter <jdstmporter@users.noreply.github.com>
2026-03-04 15:38:06 +0000

    working

--- img/basic.py
+++ img/basic.py
@@ -1,8 +1,10 @@
+import math
+
+import numpy
 import torch
 from torchvision.io import decode_image
 from torchvision.models.detection import fasterrcnn_resnet50_fpn, FasterRCNN_ResNet50_FPN_Weights
-from PIL import Image, ImageDraw
-
+from PIL import Image, ImageDraw, ImageFont
 
 img = decode_image("image.jpg")
 
@@ -27,22 +29,42 @@ labels = predictions[0]['labels']
 scores = predictions[0]['scores']
 
 person_class_id = 1
-confidence_threshold = 0.5
+confidence_threshold = 0.3
 person_indices = (labels == person_class_id) & (scores > confidence_threshold)
 person_boxes = boxes[person_indices]
+person_scores = scores[person_indices]
 
-print(f'Found {len(person_boxes)}')
-for box in person_boxes:
-    box = box.numpy().astype(int)
-    print(f'({box[0]}, {box[1]}),({box[2]}, {box[3]})')
+ma = torch.max(person_scores)
+mi = torch.min(person_scores)
+scores_norm = (person_scores - mi)/(ma - mi)
+
+zipped = list(zip(person_boxes,person_scores,scores_norm))
+
+print(f'Found {len(zipped)}')
+for bs in zipped:
+    box = bs[0].numpy().astype(int)
+    print(f'({box[0]}, {box[1]}),({box[2]}, {box[3]}) : {bs[1]}, {bs[2]}')
+
+probs = [ bs[1]*100.0 for bs in zipped]
+histo, edges = numpy.histogram(probs, bins=10,range=(0,100))
+for n in range(len(histo)):
+    h=histo[n]
+    if h>0 :
+        print(f'{edges[n]}: {h}')
 
 with Image.open('image.jpg') as annotate:
+    font = ImageFont.truetype('Arial.ttf', 18)
     draw = ImageDraw.Draw(annotate,'RGBA')
-    for box in person_boxes:
-        box = box.numpy().astype(int)
-        draw.rectangle([(box[0],box[1]),(box[2],box[3])], fill=(255,0,0,127),outline=(127,0,0))
+    for bs in zipped:
+        box = bs[0].numpy().astype(int)
+        p = int(round(float(bs[2]),2) *255)
+        prob = int(bs[1]*100)
 
-    annotate.save('annotated.jpg')
+        col = (255-p,p,0,127)
+        draw.rectangle([(box[0],box[1]),(box[2],box[3])], fill=col,outline=(0,0,0))
+        draw.text((box[0]+5,box[1]-5),f'p={prob/100.0}',font=font,font_size=8,  stroke_fill=(0,0,0))
+
+    annotate.save('annotate2.jpg')
 
 
 
